6e2e8fa675b656048b2f8f23804afcb8a120f43d
valeriYanev01 <valeri.t.yanev@gmail.com>
2024-04-22 18:30:58 +0300

    started working on the delivery modal

--- frontend/src/components/Modal/Delivery/DeliveryModal.tsx
+++ frontend/src/components/Modal/Delivery/DeliveryModal.tsx
@@ -1,7 +1,35 @@
+import React from "react";
 import "./DeliveryModal.css";
 
-const DeliveryModal = () => {
-  return <div>DeliveryModal</div>;
+const DeliveryModal: React.FC = () => {
+  return (
+    <div className="delivery-modal">
+      <div className="dm-heading">
+        <img src="/svg/decorLeftRed.svg" className="deal-decor" />
+        <p>SELECT ADDRESS & DELIVERY TIME</p>
+        <img src="/svg/decorRightRed.svg" className="deal-decor" />
+      </div>
+
+      <div className="dm-adress-container">
+        <p>Recently Selected</p>
+        <div>
+          <img />
+
+          <p>adress name</p>
+          <div>
+            <img />
+            <p>adress</p>
+          </div>
+          <div>
+            <img />
+            <p>Your Store: store</p>
+          </div>
+
+          <img />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DeliveryModal;
