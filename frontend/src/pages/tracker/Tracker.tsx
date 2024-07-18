788b3ea76c43c155ee66a7dc30daf48f88d5fd7e
valeriYanev01 <valeri.t.yanev@gmail.com>
2024-07-18 11:38:03 +0300

    finished the tracker

--- frontend/src/pages/tracker/Tracker.tsx
+++ frontend/src/pages/tracker/Tracker.tsx
@@ -3,9 +3,9 @@ import "./Tracker.css";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 import Heading from "../../components/Heading/Heading";
-import axios from "axios";
-import { LoginContext } from "../../context/LoginContext";
 import { OrderContext } from "../../context/OrderContext";
+import { useNavigate } from "react-router-dom";
+import { ModalContext } from "../../context/ModalContext";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const reducer = (state: any, action: { type: string }) => {
@@ -14,31 +14,31 @@ const reducer = (state: any, action: { type: string }) => {
       return {
         ...state,
         path1: "#006FA6",
-        text: "Preparing",
+        text: "We received your order",
       };
     case "path2":
       return {
         ...state,
         path2: "#006FA6",
-        text: "Cooking",
+        text: "Your order is being prepared",
       };
     case "path3":
       return {
         ...state,
         path3: "#006FA6",
-        text: "Oven",
+        text: "The products are in the oven. It takes 5-7 minutes",
       };
     case "path4":
       return {
         ...state,
         path4: "#006FA6",
-        text: "Checking",
+        text: "We are checking the quality of the products",
       };
     case "path5":
       return {
         ...state,
         path5: "#006FA6",
-        text: "Delivery",
+        text: "Your order is being delivered",
       };
     case "path6":
       return {
@@ -52,7 +52,8 @@ const reducer = (state: any, action: { type: string }) => {
 };
 
 const Tracker: React.FC = () => {
-  const [currentSecond, setCurrentSecond] = useState(0);
+  const [currentSecond, setCurrentSecond] = useState(new Date().getTime());
+  const [activePath, setActivePath] = useState("");
 
   const [state, dispatch] = useReducer(reducer, {
     text: "START",
@@ -63,51 +64,69 @@ const Tracker: React.FC = () => {
     path5: "#f2f4f5",
   });
 
-  const { token, emailLogin } = useContext(LoginContext);
-  const { setOrderTime, setActiveOrder, activeOrder } = useContext(OrderContext);
+  const { activeTracker } = useContext(OrderContext);
+  const { setOpenModal, setModalType } = useContext(ModalContext);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if (activeOrder) {
+    if (
+      activeTracker ||
+      (localStorage.getItem("active-tracker") &&
+        JSON.parse(localStorage.getItem("active-tracker") as string).active === true)
+    ) {
+      let finishTime;
+      let startTime;
+
+      if (localStorage.getItem("active-tracker")) {
+        finishTime = JSON.parse(localStorage.getItem("active-tracker") as string).finish;
+        startTime = JSON.parse(localStorage.getItem("active-tracker") as string).start;
+      }
+
+      const now = new Date().getTime();
+      const timeToDelivery = finishTime - startTime;
+
       const interval = setInterval(() => {
-        setCurrentSecond((prevState) => prevState + 1);
+        setCurrentSecond(currentSecond + 1000);
       }, 1000);
-      if (currentSecond >= 0) dispatch({ type: "path1" });
-      if (currentSecond >= 5) dispatch({ type: "path2" });
-      if (currentSecond >= 10) dispatch({ type: "path3" });
-      if (currentSecond >= 15) dispatch({ type: "path4" });
-      if (currentSecond >= 20) dispatch({ type: "path5" });
-      if (currentSecond >= 30) dispatch({ type: "path6" });
+
+      if (now >= startTime + timeToDelivery - 1000 * 60 * 30) {
+        dispatch({ type: "path1" });
+        setActivePath("path1");
+      } else {
+        // if its ordered for more than 30 minutes
+        dispatch({ type: "path1" });
+        setActivePath("path1");
+      }
+      if (now >= startTime + timeToDelivery - 1000 * 60 * 28) {
+        dispatch({ type: "path2" });
+        setActivePath("path2");
+      }
+      if (now >= startTime + timeToDelivery - 1000 * 60 * 22) {
+        dispatch({ type: "path3" });
+        setActivePath("path3");
+      }
+      if (now >= startTime + timeToDelivery - 1000 * 60 * 17) {
+        dispatch({ type: "path4" });
+        setActivePath("path4");
+      }
+      if (now >= startTime + timeToDelivery - 1000 * 60 * 10) {
+        dispatch({ type: "path5" });
+        setActivePath("path5");
+      }
+      if (now >= startTime + timeToDelivery) {
+        dispatch({ type: "path6" });
+        setActivePath("");
+      }
+      if (now >= startTime + timeToDelivery + 1) {
+        localStorage.setItem("active-tracker", JSON.stringify({ active: false }));
+        navigate("/");
+        window.location.reload();
+      }
+
       return () => clearInterval(interval);
     }
-  }, [currentSecond, activeOrder]);
-
-  useEffect(() => {
-    if (token && emailLogin) {
-      const getAllOrders = async () => {
-        const response = await axios.get("http://localhost:3000/api/users/get-orders", {
-          headers: { Authorization: `Bearer ${token}` },
-          params: { email: emailLogin },
-        });
-
-        const allOrders = response.data.allOrders.orders;
-        const lastOrder = new Date(allOrders[allOrders.length - 1].createdAt);
-        if (new Date() < new Date(lastOrder.getTime() + 120 * 60 * 1000)) {
-          setActiveOrder(true);
-          setOrderTime(lastOrder);
-        }
-        // const finishOrder = new Date(lastOrder.getTime() + 30 * 60 * 1000); // add * 60
-
-        // need to get the lastOrder.createdAt and to check the formula 30m / 5 * n -> where n is the step(path) and to update
-        // the circle tracker. Will update with seconds -> 30 minutes * 60 seconds to make it 1800 seconds. Now -> 1800 / 5 * n and for every
-        // n need to update the next path's color.
-
-        //  x % y === 0
-        // 5 % 5 === 0 // true
-        // 5 % 2 === 0 // false -> 1
-      };
-      getAllOrders();
-    }
-  }, [emailLogin, setOrderTime, token, setActiveOrder]);
+  }, [activeTracker, currentSecond, navigate]);
 
   return (
     <div className="tracker">
@@ -120,45 +139,72 @@ const Tracker: React.FC = () => {
             <p>|</p>
             <p>{state.text}</p>
           </div>
-          <svg
-            version="1.1"
-            baseProfile="tiny"
-            className="tracker-circle no-order"
-            data-step="0"
-            id="Layer_1"
-            xmlns="http://www.w3.org/2000/svg"
-            x="0px"
-            y="0px"
-            viewBox="0 0 1080 1080"
-          >
-            <path
-              id="piece1"
-              fill={state.path6 ? state.path6 : state.path1}
-              d="M662,135.3l37.8-47L695.7,35c-48.3-14.9-99.4-23-152.3-23.4v105.9C584.7,117.8,624.4,123.9,662,135.3z"
-            ></path>
-            <path
-              id="piece2"
-              fill={state.path6 ? state.path6 : state.path2}
-              d="M676.9,140c162.8,56,281.2,208.6,285.7,389.2l47.5,19.7l58.5-21.9C1063.1,301,915,109.6,710.9,39.7l4.1,53.1L676.9,140z"
-            ></path>
-            <path
-              id="piece3"
-              fill={state.path6 ? state.path6 : state.path3}
-              d="M1068.6,542.5l-58.8,22.1L962.6,545C959.8,775.2,771.9,961.9,541,962.4l-20.8,53l21,52.8C831.3,1067.7,1067.2,832.5,1068.6,542.5z"
-            ></path>
-            <path
-              id="piece4"
-              fill={state.path6 ? state.path6 : state.path4}
-              d="M525.5,962.2c-108.8-3.8-207.2-48.7-280.1-119.7l-56.1,21.9L166.1,913c92.5,92.8,219.3,151.4,359.4,155.3l-20.8-52.7L525.5,962.2z"
-            ></path>
-            <path
-              id="piece5"
-              fill={state.path6 ? state.path6 : state.path5}
-              d="M234.1,831.2c-72.3-75.9-116.8-178.5-116.8-291.4c0-229.3,183.5-416.6,411.4-422.4V11.7C242.6,17.6,11.4,252.4,11.4,540c0,139.8,54.7,267.2,143.7,361.7l23.4-48.9L234.1,831.2z"
-            ></path>
-          </svg>
+          <div className="tracker-circle-container">
+            <svg
+              version="1.1"
+              baseProfile="tiny"
+              className="tracker-circle no-order"
+              data-step="0"
+              id="Layer_1"
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              viewBox="0 0 1080 1080"
+            >
+              <path
+                className={`${activePath === "path1" ? "tracker-path-animation" : ""}`}
+                id="piece1"
+                fill={state.path6 ? state.path6 : state.path1}
+                d="M662,135.3l37.8-47L695.7,35c-48.3-14.9-99.4-23-152.3-23.4v105.9C584.7,117.8,624.4,123.9,662,135.3z"
+              ></path>
+              <path
+                className={`${activePath === "path2" ? "tracker-path-animation" : ""}`}
+                id="piece2"
+                fill={state.path6 ? state.path6 : state.path2}
+                d="M676.9,140c162.8,56,281.2,208.6,285.7,389.2l47.5,19.7l58.5-21.9C1063.1,301,915,109.6,710.9,39.7l4.1,53.1L676.9,140z"
+              ></path>
+              <path
+                className={`${activePath === "path3" ? "tracker-path-animation" : ""}`}
+                id="piece3"
+                fill={state.path6 ? state.path6 : state.path3}
+                d="M1068.6,542.5l-58.8,22.1L962.6,545C959.8,775.2,771.9,961.9,541,962.4l-20.8,53l21,52.8C831.3,1067.7,1067.2,832.5,1068.6,542.5z"
+              ></path>
+              <path
+                className={`${activePath === "path4" ? "tracker-path-animation" : ""}`}
+                id="piece4"
+                fill={state.path6 ? state.path6 : state.path4}
+                d="M525.5,962.2c-108.8-3.8-207.2-48.7-280.1-119.7l-56.1,21.9L166.1,913c92.5,92.8,219.3,151.4,359.4,155.3l-20.8-52.7L525.5,962.2z"
+              ></path>
+              <path
+                className={`${activePath === "path5" ? "tracker-path-animation" : ""}`}
+                id="piece5"
+                fill={state.path6 ? state.path6 : state.path5}
+                d="M234.1,831.2c-72.3-75.9-116.8-178.5-116.8-291.4c0-229.3,183.5-416.6,411.4-422.4V11.7C242.6,17.6,11.4,252.4,11.4,540c0,139.8,54.7,267.2,143.7,361.7l23.4-48.9L234.1,831.2z"
+              ></path>
+            </svg>
+            {(localStorage.getItem("active-tracker") &&
+              !JSON.parse(localStorage.getItem("active-tracker") as string).active) ||
+              (activeTracker === false && (
+                <div className="tracker-new-order">
+                  <div>
+                    <p className="tracker-new-order-text">You don't have an active order.</p>
+                    <br />
+                    <span
+                      onClick={() => {
+                        setOpenModal(true);
+                        setModalType("method");
+                      }}
+                      className="tracker-new-order-btn"
+                    >
+                      ORDER
+                    </span>
+                  </div>
+                </div>
+              ))}
+          </div>
         </div>
       </div>
+
       <Footer />
     </div>
   );
