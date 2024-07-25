df3a3b0e9f3e8e8b9a5b4c2cce19dfaecda6c888
valeriYanev01 <valeri.t.yanev@gmail.com>
2024-07-25 10:38:53 +0300

    added message if there is active order

--- frontend/src/components/Tracker/TrackerComponent.tsx
+++ frontend/src/components/Tracker/TrackerComponent.tsx
@@ -178,7 +178,7 @@ const TrackerComponent: React.FC = () => {
               d="M234.1,831.2c-72.3-75.9-116.8-178.5-116.8-291.4c0-229.3,183.5-416.6,411.4-422.4V11.7C242.6,17.6,11.4,252.4,11.4,540c0,139.8,54.7,267.2,143.7,361.7l23.4-48.9L234.1,831.2z"
             ></path>
           </svg>
-          {!activeTracker ? (
+          {!activeTracker && !activeOrder ? (
             <div className="tracker-new-order">
               <div>
                 <p className="tracker-new-order-text">You don't have an active order.</p>
@@ -195,14 +195,17 @@ const TrackerComponent: React.FC = () => {
               </div>
             </div>
           ) : (
-            activeOrder &&
-            !activeTracker && (
-              <div className="tracker-new-order">
+            !activeTracker &&
+            activeOrder && (
+              <div className="tracker-continue-order">
                 <div>
-                  <p className="tracker-new-order-text">Continue with your order.</p>
+                  <p className="tracker-continue-order-text">Continue with your order.</p>
                   <br />
-                  <Link to={`/menu/${orderStore.toLocaleLowerCase().split(" ").join("")}/pizza`}>
-                    <span className="tracker-new-order-btn">CONTINUE</span>
+                  <Link
+                    to={`/menu/${orderStore.toLocaleLowerCase().split(" ").join("")}/pizza`}
+                    className="tracker-continue-order-link"
+                  >
+                    <span className="tracker-continue-order-btn">CONTINUE</span>
                   </Link>
                 </div>
               </div>
