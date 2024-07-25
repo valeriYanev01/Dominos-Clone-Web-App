763ba2980737258dd96cd3e0dc181d09ba84f24a
valeriYanev01 <valeri.t.yanev@gmail.com>
2024-07-25 10:04:48 +0300

    extracted the content from the page into seperate component

---
+++ frontend/src/components/Careers/CareersComponent.tsx
@@ -0,0 +1,311 @@
+import React, { useState } from "react";
+import "./CareersComponent.css";
+import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
+import axios from "axios";
+
+const CareersComponent: React.FC = () => {
+  const [name, setName] = useState("");
+  const [city, setCity] = useState("");
+  const [number, setNumber] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+  const [email, setEmail] = useState("");
+  const [position, setPosition] = useState("");
+  const [cv, setCv] = useState<File | null>(null);
+  const [cvValue, setCvValue] = useState("");
+  const [firstCheckboxChecked, setFirstCheckboxChecked] = useState(false);
+  const [secondCheckboxChecked, setSecondCheckboxChecked] = useState(false);
+  const [error, setError] = useState("");
+  const [isApplying, setIsApplying] = useState(false);
+  const [successfulApplied, setSuccessfullApplied] = useState(false);
+  const [btnText, setBtnText] = useState("SUBMIT");
+
+  const { executeRecaptcha } = useGoogleReCaptcha();
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    setIsApplying(true);
+    setBtnText("SUBMITTING");
+
+    if (!executeRecaptcha) {
+      setError("Execute reCAPTCHA not available yet");
+      return;
+    }
+
+    const recaptchaToken = await executeRecaptcha("apply");
+
+    if (recaptchaToken) {
+      try {
+        setError("");
+
+        const response = await axios.post("http://localhost:3000/api/users/verify-google-recaptcha-token", {
+          recaptchaToken: recaptchaToken,
+        });
+
+        const result = response.data;
+
+        if (result.success) {
+          try {
+            if (!cv) {
+              setError("Please upload your CV");
+              return;
+            }
+            setError("");
+
+            const formData = new FormData();
+
+            formData.append("cv", cv);
+            formData.append("name", name);
+            formData.append("city", city);
+            formData.append("number", number);
+            formData.append("birthDate", birthDate);
+            formData.append("email", email);
+            formData.append("position", position);
+
+            const response = await axios.post("http://localhost:3000/api/users/apply", formData, {
+              headers: { "Content-Type": "multipart/form-data" },
+            });
+            const result = response.data;
+
+            if (result.success) {
+              setName("");
+              setCity("");
+              setNumber("");
+              setBirthDate("");
+              setEmail("");
+              setPosition("");
+              setCv(null);
+              setCvValue("");
+              setFirstCheckboxChecked(false);
+              setSecondCheckboxChecked(false);
+              setError("");
+
+              window.scrollTo(0, 0);
+
+              setSuccessfullApplied(true);
+              setBtnText("SUBMITTED ✔");
+            } else {
+              setError("Something went wrong.");
+            }
+          } catch (err) {
+            if (axios.isAxiosError(err)) {
+              setError(err.message);
+            }
+          }
+        } else {
+          setError(`reCAPTCHA verification failed", ${result["error-codes"]}`);
+        }
+      } catch (err) {
+        if (axios.isAxiosError(err)) {
+          setError(`Error verifying reCAPTCHA:, ${err.message}`);
+        }
+      } finally {
+        setIsApplying(false);
+      }
+    }
+  };
+
+  return (
+    <div className="careers-container">
+      <div className="careers-heading">
+        <img className="deal-decor" src="/svg/decorLeftRed.svg" />
+        <p>JOB APPLICATION</p>
+        <img className="deal-decor" src="/svg/decorRightRed.svg" />
+      </div>
+
+      <div className="careers-body">
+        <p className="careers-body-text">
+          Making and delivering a pizza in about 30 minutes is a big challenge. In order to help the people who work at
+          Domino’s to develop their talent and reach their potential, we offer:
+        </p>
+
+        <ul className="careers-body-benefits">
+          <li>- Worldwide acknowledged training programs</li>
+          <li>- Competitive remuneration</li>
+          <li>- Flexible working schedule</li>
+          <li>- Opportunity for a long-term career and growth in the company</li>
+        </ul>
+
+        <p className="careers-body-text">
+          If you wish to be a part of our winning team, to represent Domino’s brand, to deliver security, good service
+          and delicious food to our customers, please fill in the form below or send your CV to jobs@dominos.bg and we
+          will invite you to an interview.
+        </p>
+
+        <form onSubmit={handleSubmit} className="careers-form">
+          <div className="careers-jobs-container">
+            <p>JOB OPPORTUNITIES</p>
+
+            <div className="careers-jobs">
+              <p>Delivery Drivers</p>
+              <p>Insiders - Kitchen Staff</p>
+            </div>
+          </div>
+
+          <div className="careers-personal-details">
+            <p>PERSONAL DETAILS</p>
+            <div className="careers-personal-details-field-single">
+              <label htmlFor="careers-name">NAME *</label>
+              <input id="careers-name" type="text" required value={name} onChange={(e) => setName(e.target.value)} />
+            </div>
+
+            <div className="careers-personal-details-field-container">
+              <div className="careers-personal-details-field">
+                <label htmlFor="city">CITY *</label>
+                <input id="city" type="text" required value={city} onChange={(e) => setCity(e.target.value)} />
+              </div>
+
+              <div className="careers-personal-details-field">
+                <label htmlFor="phone-number">TELEPHONE NUMBER *</label>
+                <input
+                  id="phone-number"
+                  type="text"
+                  required
+                  value={number}
+                  onChange={(e) => setNumber(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div className="careers-personal-details-field-container">
+              <div className="careers-personal-details-field">
+                <label htmlFor="birth-date">DATE OF BIRTH *</label>
+                <input
+                  id="birth-date"
+                  type="date"
+                  required
+                  value={birthDate}
+                  onChange={(e) => setBirthDate(e.target.value)}
+                />
+              </div>
+
+              <div className="careers-personal-details-field">
+                <label htmlFor="email">EMAIL</label>
+                <input id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+              </div>
+            </div>
+          </div>
+
+          <div className="careers-positions-outer-container">
+            <p className="careers-positions-text">I WOULD LIKE TO APPLY FOR: *</p>
+
+            <div className="careers-positions-container">
+              <div className="careers-positions">
+                <input
+                  id="delivery"
+                  type="radio"
+                  name="job"
+                  value="Delivery Drivers"
+                  required
+                  checked={position === "Delivery Drivers"}
+                  onChange={() => setPosition("Delivery Drivers")}
+                />
+                <label htmlFor="delivery">Delivery Drivers</label>
+              </div>
+
+              <div className="careers-positions">
+                <input
+                  id="kitchen"
+                  type="radio"
+                  name="job"
+                  value="Kitchen Staff"
+                  required
+                  checked={position === "Kitchen Staff"}
+                  onChange={() => setPosition("Kitchen Staff")}
+                />
+                <label htmlFor="kitchen">Kitchen Staff</label>
+              </div>
+
+              <div className="careers-positions">
+                <input
+                  id="either"
+                  type="radio"
+                  name="job"
+                  value="Either One"
+                  required
+                  checked={position === "Either One"}
+                  onChange={() => setPosition("Either One")}
+                />
+                <label htmlFor="either">Either One</label>
+              </div>
+            </div>
+          </div>
+
+          <div className="careers-cv-conditions-container">
+            <div className="careers-upload-cv">
+              <p>SEND US YOUR CV *</p>
+              <input
+                value={cvValue}
+                required
+                type="file"
+                accept=".doc, .docx, .pdf, .txt, .image/*"
+                onChange={(e) => {
+                  if (e.target.files && e.target.files[0]) {
+                    setCvValue(e.target.value);
+                    if (e.target.files[0].size > 1024 * 1024 * 2) {
+                      e.target.value = "";
+                      setCv(null);
+                      setError("Uploading unsuccessful - file too big.");
+                    } else {
+                      setError("");
+                      setCv(e.target.files[0]);
+                    }
+                  }
+                }}
+              />
+            </div>
+
+            <div className="careers-conditions-container">
+              <div className="careers-conditions">
+                <input
+                  type="checkbox"
+                  id="cond1"
+                  onChange={() => setFirstCheckboxChecked(!firstCheckboxChecked)}
+                  checked={firstCheckboxChecked}
+                  required
+                />
+                <label htmlFor="cond1">
+                  By applying for a job at LN Solutions LTD I hereby confirm that I have read the Job Application Data
+                  Collection and Recording Policy and I desire that the administrator company LN Solution Ltd collects
+                  and processes the personal data that I submit by completing the form at www.dominos.bg/career as well
+                  as the personal data that I have sent in the CV.
+                </label>
+              </div>
+
+              <div className="careers-conditions">
+                <input
+                  type="checkbox"
+                  id="cond2"
+                  onChange={() => setSecondCheckboxChecked(!secondCheckboxChecked)}
+                  checked={secondCheckboxChecked}
+                  required
+                />
+                <label htmlFor="cond2">
+                  I confirm that I have read and I agree with the Statement of Consent for Personal Data Processing
+                  Related to Job Applications
+                </label>
+              </div>
+            </div>
+          </div>
+
+          <p className="careers-required-fields-text">All fields marked with ( *) are </p>
+
+          <div className="careers-btn-container">
+            <button
+              type="submit"
+              className={`careers-btn ${isApplying || successfulApplied ? "careers-btn-disabled" : ""}`}
+              disabled={isApplying || successfulApplied}
+            >
+              {btnText} <span className={`${isApplying ? "careers-btn-text-dot-animation" : ""}`}></span>
+            </button>
+          </div>
+
+          {error && <p>{error}</p>}
+          {successfulApplied && <div className="careers-success">Application Submitted!</div>}
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CareersComponent;
---
+++ frontend/src/components/Tracker/TrackerComponent.tsx
@@ -0,0 +1,217 @@
+import React, { useContext, useEffect, useReducer, useState } from "react";
+import "./TrackerComponent.css";
+import { OrderContext } from "../../context/OrderContext";
+import { Link, useNavigate } from "react-router-dom";
+import { ModalContext } from "../../context/ModalContext";
+import Heading from "../Heading/Heading";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const reducer = (state: any, action: { type: string }) => {
+  switch (action.type) {
+    case "path1":
+      return {
+        ...state,
+        path1: "#006FA6",
+        text: "We received your order",
+      };
+    case "path2":
+      return {
+        ...state,
+        path2: "#006FA6",
+        text: "Your order is being prepared",
+      };
+    case "path3":
+      return {
+        ...state,
+        path3: "#006FA6",
+        text: "The products are in the oven. It takes 5-7 minutes",
+      };
+    case "path4":
+      return {
+        ...state,
+        path4: "#006FA6",
+        text: "We are checking the quality of the products",
+      };
+    case "path5":
+      return {
+        ...state,
+        path5: "#006FA6",
+        text: "Your order is being delivered",
+      };
+    case "path6":
+      return {
+        ...state,
+        path6: "#609e29",
+        text: "Delivered!",
+      };
+    default:
+      return state;
+  }
+};
+
+const TrackerComponent: React.FC = () => {
+  const [currentSecond, setCurrentSecond] = useState(new Date().getTime());
+  const [activePath, setActivePath] = useState("");
+
+  const { activeTracker, activeOrder, orderStore } = useContext(OrderContext);
+  const { setOpenModal, setModalType } = useContext(ModalContext);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (
+      activeTracker ||
+      (localStorage.getItem("active-tracker") &&
+        JSON.parse(localStorage.getItem("active-tracker") as string).active === true)
+    ) {
+      let finishTime;
+      let startTime;
+
+      if (localStorage.getItem("active-tracker")) {
+        finishTime = JSON.parse(localStorage.getItem("active-tracker") as string).finish;
+        startTime = JSON.parse(localStorage.getItem("active-tracker") as string).start;
+      }
+
+      const now = new Date().getTime();
+      const timeToDelivery = finishTime - startTime;
+
+      const interval = setInterval(() => {
+        setCurrentSecond(currentSecond + 1000);
+      }, 1000);
+
+      if (now >= startTime + timeToDelivery - 1000 * 60 * 30) {
+        dispatch({ type: "path1" });
+        setActivePath("path1");
+      } else {
+        // if its ordered for more than 30 minutes
+        dispatch({ type: "path1" });
+        setActivePath("path1");
+      }
+      if (now >= startTime + timeToDelivery - 1000 * 60 * 28) {
+        dispatch({ type: "path2" });
+        setActivePath("path2");
+      }
+      if (now >= startTime + timeToDelivery - 1000 * 60 * 22) {
+        dispatch({ type: "path3" });
+        setActivePath("path3");
+      }
+      if (now >= startTime + timeToDelivery - 1000 * 60 * 17) {
+        dispatch({ type: "path4" });
+        setActivePath("path4");
+      }
+      if (now >= startTime + timeToDelivery - 1000 * 60 * 10) {
+        dispatch({ type: "path5" });
+        setActivePath("path5");
+      }
+      if (now >= startTime + timeToDelivery) {
+        dispatch({ type: "path6" });
+        setActivePath("");
+      }
+      if (now >= startTime + timeToDelivery + 1) {
+        localStorage.setItem("active-tracker", JSON.stringify({ active: false }));
+        navigate("/");
+        window.location.reload();
+      }
+
+      return () => clearInterval(interval);
+    }
+  }, [activeTracker, currentSecond, navigate]);
+
+  const [state, dispatch] = useReducer(reducer, {
+    text: "START",
+    path1: "#f2f4f5",
+    path2: "#f2f4f5",
+    path3: "#f2f4f5",
+    path4: "#f2f4f5",
+    path5: "#f2f4f5",
+  });
+
+  return (
+    <div className="tracker-body">
+      <Heading text="PIZZA TRACKER: TRACK YOUR ORDER REAL-TIME!" />
+
+      <div className="tracker-svg-container">
+        <div className="tracker-svg-text">
+          <p>|</p>
+          <p>{state.text}</p>
+        </div>
+        <div className="tracker-circle-container">
+          <svg
+            version="1.1"
+            baseProfile="tiny"
+            className="tracker-circle no-order"
+            data-step="0"
+            id="Layer_1"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            viewBox="0 0 1080 1080"
+          >
+            <path
+              className={`${activePath === "path1" ? "tracker-path-animation" : ""}`}
+              id="piece1"
+              fill={state.path6 ? state.path6 : state.path1}
+              d="M662,135.3l37.8-47L695.7,35c-48.3-14.9-99.4-23-152.3-23.4v105.9C584.7,117.8,624.4,123.9,662,135.3z"
+            ></path>
+            <path
+              className={`${activePath === "path2" ? "tracker-path-animation" : ""}`}
+              id="piece2"
+              fill={state.path6 ? state.path6 : state.path2}
+              d="M676.9,140c162.8,56,281.2,208.6,285.7,389.2l47.5,19.7l58.5-21.9C1063.1,301,915,109.6,710.9,39.7l4.1,53.1L676.9,140z"
+            ></path>
+            <path
+              className={`${activePath === "path3" ? "tracker-path-animation" : ""}`}
+              id="piece3"
+              fill={state.path6 ? state.path6 : state.path3}
+              d="M1068.6,542.5l-58.8,22.1L962.6,545C959.8,775.2,771.9,961.9,541,962.4l-20.8,53l21,52.8C831.3,1067.7,1067.2,832.5,1068.6,542.5z"
+            ></path>
+            <path
+              className={`${activePath === "path4" ? "tracker-path-animation" : ""}`}
+              id="piece4"
+              fill={state.path6 ? state.path6 : state.path4}
+              d="M525.5,962.2c-108.8-3.8-207.2-48.7-280.1-119.7l-56.1,21.9L166.1,913c92.5,92.8,219.3,151.4,359.4,155.3l-20.8-52.7L525.5,962.2z"
+            ></path>
+            <path
+              className={`${activePath === "path5" ? "tracker-path-animation" : ""}`}
+              id="piece5"
+              fill={state.path6 ? state.path6 : state.path5}
+              d="M234.1,831.2c-72.3-75.9-116.8-178.5-116.8-291.4c0-229.3,183.5-416.6,411.4-422.4V11.7C242.6,17.6,11.4,252.4,11.4,540c0,139.8,54.7,267.2,143.7,361.7l23.4-48.9L234.1,831.2z"
+            ></path>
+          </svg>
+          {!activeTracker ? (
+            <div className="tracker-new-order">
+              <div>
+                <p className="tracker-new-order-text">You don't have an active order.</p>
+                <br />
+                <span
+                  onClick={() => {
+                    setOpenModal(true);
+                    setModalType("method");
+                  }}
+                  className="tracker-new-order-btn"
+                >
+                  ORDER
+                </span>
+              </div>
+            </div>
+          ) : (
+            activeOrder &&
+            !activeTracker && (
+              <div className="tracker-new-order">
+                <div>
+                  <p className="tracker-new-order-text">Continue with your order.</p>
+                  <br />
+                  <Link to={`/menu/${orderStore.toLocaleLowerCase().split(" ").join("")}/pizza`}>
+                    <span className="tracker-new-order-btn">CONTINUE</span>
+                  </Link>
+                </div>
+              </div>
+            )
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TrackerComponent;
